42fb9168e9906b0649891dbe0ab0d7e65d733bc7
Inpyo Jeon <inpyojeon@hyundaihmall.com>
2022-07-06 15:32:06 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Lnb from './Layouts/Lnb'
+import Header from './Layouts/Header'
+import Main from './Layouts/Main'
+
+function App() {
+  return (
+    <div className="root">
+      <Lnb/>
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
